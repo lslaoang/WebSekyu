14963c8a8159a1f0c84e94f90a1629d32352a268
Laurel Lao Ang <laurellaoang@gmail.com>
2022-11-19 12:42:46 +0800

    whoami added

--- src/main/java/com/lao/websekyu/controller/PageController.java
+++ src/main/java/com/lao/websekyu/controller/PageController.java
@@ -2,6 +2,9 @@ package com.lao.websekyu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 @Controller
 public class PageController {
@@ -22,5 +25,12 @@ public class PageController {
     public String getSuccessLoginPage(){
         System.out.println("This happened!");
         return "login-success";
+    }
+
+    @GetMapping("/whoami")
+    @ResponseBody
+    public Principal getRequestorInfo(Principal principal){
+        System.out.println("INFO: " + principal);
+        return principal;
     }
 }
