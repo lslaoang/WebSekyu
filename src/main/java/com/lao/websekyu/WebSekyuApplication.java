04acff223d297d2616f4873b019a5827db72b415
Laurel Lao Ang <laurellaoang@gmail.com>
2022-11-19 11:49:41 +0800

    Initial commit

---
+++ src/main/java/com/lao/websekyu/WebSekyuApplication.java
@@ -0,0 +1,13 @@
+package com.lao.websekyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSekyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSekyuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lao/websekyu/WebSekyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lao.websekyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSekyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
