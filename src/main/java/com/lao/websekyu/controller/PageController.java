b25aa00ee027f1e90d3fcf6bfb5e471d0bca14f0
Laurel Lao Ang <laurellaoang@gmail.com>
2022-11-21 11:17:19 +0800

    add logger

--- src/main/java/com/lao/websekyu/controller/PageController.java
+++ src/main/java/com/lao/websekyu/controller/PageController.java
@@ -9,29 +9,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
+import java.util.logging.Logger;
 
 @Controller
 public class PageController {
 
+    private static final Logger LOGGER = Logger.getLogger(PageController.class.getName());
+
     @GetMapping("/login")
     public String getLoginPage(){
+        LOGGER.info("Authenticating request ...");
         return "oauth2-login";
     }
 
     @GetMapping("/access-denied")
     public String getFailedLoginPage(){
+        LOGGER.info("Access to this resource denied.");
         return "access-denied";
     }
 
     @GetMapping("/login-success")
     public String getSuccessLoginPage(){
+        LOGGER.info("Login via OAuth2 successful!");
         return "login-success";
     }
 
     @GetMapping("/whoami")
     @ResponseBody
     public ResponseEntity<String> getRequesterInfo(Principal principal) {
-
+        LOGGER.info("Fetching user's information.");
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         return new ResponseEntity<>(gson.toJson(principal), HttpStatus.ACCEPTED);
     }
